e6e96c146c2d23ff7214f51f27d2b6c2a0944b6e
Ashish <ash550556@gmail.com>
2022-10-17 10:46:27 +0530

    Final

--- Assignment-4/Q1_CheckArmstrong.cpp
+++ Assignment-4/Q1_CheckArmstrong.cpp
@@ -1,1 +1,31 @@
-df
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num,num1;
+    int x=0,y=0;
+    cout << " Enter Number to Check Number is ArmStrong Or Not";
+    cin >> num;
+    num1=num;
+
+    while(num1>0)
+    {
+        x=num1%10;
+        y+=(x*x*x);
+        num1=num1/10;
+    }
+
+    if(y==num)
+    {
+        cout << num <<" is Armstrong";
+
+    }
+    else
+    {
+        cout << num <<" is not Armstrong";
+    }
+
+    return num;
+}
